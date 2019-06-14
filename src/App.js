4154506d730a9ffb1d250b17a68648f78609a0d9
Stephen Dorn <steved11@sbcglobal.net>
2019-06-14 11:57:05 -0700

    delete unneccessary stuff

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 // import all pages
-import Adopt from "./pages/Adopt";
-import Application from "./pages/Application";
 import Contact from "./pages/Contact";
-import CorporateSponsorships from "./pages/CorporateSponsorships";
-import Donate from "./pages/Donate";
-import Foster from "./pages/Foster";
 import Home from "./pages/Home";
-import Mission from "./pages/Mission";
-import OurStory from "./pages/OurStory";
-import Surrender from "./pages/Surrender";
-import Volunteer from "./pages/Volunteer";
 
 import "./App.css";
 
@@ -39,16 +30,7 @@ const App = () => (
 
                 <div className="App">
                     <Route path="/" exact component={Home} />
-                    <Route path="/adopt" component={Adopt} />
-                    <Route path="/application" component={Application} />
                     <Route path="/contact" component={Contact} />
-                    <Route path="/corporate-sponsorships" component={CorporateSponsorships} />
-                    <Route path="/donate" component={Donate} />
-                    <Route path="/foster" component={Foster} />
-                    <Route path="/mission" component={Mission} />
-                    <Route path="/our-story" component={OurStory} />
-                    <Route path="/surrender" component={Surrender} />
-                    <Route path="/volunteer" component={Volunteer} />
                 </div>
 
                 <Footer />
