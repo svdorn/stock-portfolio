68c6cbd0f682f5552ad9339ba21e5a8287ccbd13
Stephen Dorn <stevedorn9@gmail.com>
2019-06-15 17:25:23 -0700

    graphs are responsive

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 
 import "./Graph.css";
 
@@ -9,12 +9,14 @@ const Graph = ({ data, highDomain, lowDomain, color }) => {
             {data.length === 0 ? (
                 <div className="Loading">Loading...</div>
             ) : (
-                <LineChart width={900} height={300} data={data}>
-                    <XAxis dataKey="name" />
-                    <YAxis domain={[lowDomain, highDomain]} />
-                    <Tooltip />
-                    <Line dataKey="Price" stroke={color} dot={false} />
-                </LineChart>
+                <ResponsiveContainer width="100%" height={300}>
+                    <LineChart data={data}>
+                        <XAxis dataKey="name" />
+                        <YAxis domain={[lowDomain, highDomain]} />
+                        <Tooltip />
+                        <Line dataKey="Price" stroke={color} dot={false} />
+                    </LineChart>
+                </ResponsiveContainer>
             )}
         </div>
     );
